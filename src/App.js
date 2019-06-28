f775f9c09b25d35f814eed51c806d14b0845b923
娄金峰 <loujinfeng123@gmail.com>
2019-06-28 16:14:45 +0800

    简单路由

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Menu, Icon } from 'antd';
+import Home from './pages/home/home';
+import Text from './pages/text/text';
+import About from './pages/about/about';
 
 function App() {
+  const { SubMenu } = Menu;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         测试一下首页15
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div>
+        啦啦啦
+        </div>
+
+        <div>
+          <Router>
+        <Menu mode="horizontal">
+          <Menu.Item key="Home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="text">
+            <Link to="/text">text</Link>
+          </Menu.Item>
+          <Menu.Item key="about">
+               <Link to="/about">about</Link>
+          </Menu.Item>
+        </Menu>
+        <Route path="/" component={Home} exact />
+        <Route path="/text" component={Text} />
+        <Route path="/about" component={About} />
+      </Router>
+        </div>
+  
+      </div>
+  
     </div>
   );
 }
