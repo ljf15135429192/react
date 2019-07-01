66a8c8fd45cd645354af7e66bea767b8e4565ab4
ljf <loujinfeng123@gmail.com>
2019-07-01 21:30:52 +0800

    上传build文件

---
+++ build/precache-manifest.623977fe50adc6324193be673faf566a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0d65372f9d6c776f4c16992ec9f174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598018a0204a4a8b6759",
+    "url": "/static/css/2.8c53fc68.chunk.css"
+  },
+  {
+    "revision": "67bbc0e8085e58c11f1c",
+    "url": "/static/css/main.d49523a8.chunk.css"
+  },
+  {
+    "revision": "598018a0204a4a8b6759",
+    "url": "/static/js/2.c7ddd38b.chunk.js"
+  },
+  {
+    "revision": "67bbc0e8085e58c11f1c",
+    "url": "/static/js/main.ff8cb01c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
